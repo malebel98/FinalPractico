ba787ddb6ca3f23df5b3bc4873b2041039f00da9
AndresLosada7 <andreslosada7@gmail.com>
2017-11-20 13:46:40 -0500

    UltimoLosa
    jiji

---
+++ Assets/Scripts/Camara.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour {
+
+    
+  public GameObject player;
+    private Vector3 offset;
+    private Camera cam;
+    public float field ;
+    public float max;
+    public float min;
+  
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+        offset = transform.position - player.transform.position;
+
+    }
+	
+	// Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyDown("m"))
+        {
+            cam.fieldOfView = field;
+            field = 45f;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    Animator Anim;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0, 0, -vertical) * Time.deltaTime * movementSpeed;
+        transform.Translate(movement);
+
+        Vector3 rotation = new Vector3(-horizontal, 0, 0) * movementSpeed * Time.deltaTime;
+        transform.Translate(rotation);
+
+        Anim.SetFloat("CHorizontal", horizontal);
+        Anim.SetFloat("CVertical", vertical);
+
+    }
+}
