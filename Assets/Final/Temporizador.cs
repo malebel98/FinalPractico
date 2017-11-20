e87c6e82b98e8dcf1b653c8756491fc8a91437cf
luisaloperagiraldo <luisaf.loperagiraldo@gmail.com>
2017-11-20 12:22:40 -0500

    Tiempo
    Game Over

---
+++ Assets/Final/Temporizador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Temporizador : MonoBehaviour {
+
+    public Text contador;
+    public Text fin;
+    private float tiempo = 10f;
+
+	void Start () {
+
+        contador.text = "Time: " + tiempo;
+        fin.enabled = false;
+		
+	}
+
+    private void Update()
+    {
+        tiempo -= Time.deltaTime;
+        contador.text = "Time: " + tiempo.ToString("f0");
+
+        if(tiempo <= 0)
+        {
+            contador.text = "0";
+                fin.enabled = true;
+        }
+
+    }
+
+}
